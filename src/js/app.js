efe28caae11e31b408ed6cfab5047d7466e01a59
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2024-05-16 10:02:37 +0200

    Firebase setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,78 @@
 import { handleScroll } from './header.js';
 import { revealSections } from './scrollReveal.js';
 import { toggleNavbar, closeNavbar } from './navbar.js';
+import { firebaseConfig } from "/firebaseConfig"
+import { initializeApp } from "firebase/app"
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth"
+
+initializeApp(firebaseConfig)
+
+const authService = getAuth();
+
+const emailInput = document.querySelector(".email");
+const passwordInput = document.querySelector(".password");
+const signInButton = document.querySelector(".sign-in-button");
+const signOutButton = document.querySelector(".sign-out-button");
+const signUpButton = document.querySelector(".sign-up-button");
+const errorMessage = document.querySelector(".error-message");
+
+const frontPage = document.querySelector(".front-page");
+const secretSection = document.querySelector(".secret-section");
+
+
+signUpButton.addEventListener("click", (event)=> {
+	event.preventDefault();
+	createUserWithEmailAndPassword(authService, emailInput.value, passwordInput.value)
+	.then(()=> {
+		checkAuthStateAndRender();
+	})
+	.catch((error)=> {
+		errorMessage.style.display = "block";
+		errorMessage.textContent = error.message
+	})
+})
+
+signInButton.addEventListener("click", (event)=> {
+	event.preventDefault();
+	signInWithEmailAndPassword(authService, emailInput.value, passwordInput.value)
+	.then(()=> {
+		checkAuthStateAndRender();
+	})
+	.catch((error)=> {
+		errorMessage.style.display = "block";
+		errorMessage.textContent = error.message
+	})
+})
+
+signOutButton.addEventListener("click", ()=> {
+	signOut(authService)
+	.then(()=> {
+		checkAuthStateAndRender();
+		console.log("Signed out ✔︎");
+	})
+	.catch((error)=> {
+		console.log(error.message);
+	})
+})
+
+function checkAuthStateAndRender() {
+	onAuthStateChanged(authService, (user)=> {
+		if(user) {
+			errorMessage.textContent = ""
+			frontPage.style.display = "none";
+			secretSection.style.display = "block";
+			signOutButton.style.display = "block";
+		} else {
+			frontPage.style.display = "block";
+			secretSection.style.display = "none";
+			signOutButton.style.display = "none";
+		}
+	})
+}
+
+checkAuthStateAndRender()
+
+/*
 
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
@@ -20,4 +92,4 @@ addEventOnElem(navLinks, "click", closeNavbar);
 
 window.addEventListener("scroll", handleScroll);
 revealSections();
-addEventOnElem(window, "scroll", revealSections);
+addEventOnElem(window, "scroll", revealSections); */
---
+++ src/js/signInValidation.js
@@ -0,0 +1,9 @@
+const validateSignInForm = (email, password, emailError, passError) => {
+	const errors = {
+		errorStatus: false,
+		emailError: 'Email is required',
+		passwordError: 'Password is required'
+	}
+}
+
+// validate form, ca 41:00 i authentication part 1
